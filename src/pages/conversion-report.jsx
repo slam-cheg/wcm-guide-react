539940ceef0a934c3b95c9a28a2f7c52ca1bd2af
Lev Shushkov <levashushkov@yandex.ru>
2022-03-05 18:17:22 +0300

    lead fix

--- src/pages/conversion-report.jsx
+++ src/pages/conversion-report.jsx
@@ -12,9 +12,6 @@ function ConversionReport() {
     const styles = {
         marginTop: 10,
     };
-    const style = {
-        justifyContent: "flex-end"
-    }
     return (
         <div className="section__wrapper">
             <section className="section section__not-homepage" id="conversion-report">
@@ -22,42 +19,37 @@ function ConversionReport() {
                     <BackButton />
                     <h1 className="section-heading__title">Отчеты по конверсиям</h1>
                 </div>
-                <div className="section__grid-block">
-                    <div className="section__content">
-                        <p className="section__lead" style={style}>
-                            При переходе на&nbsp;вкладку Conversion в&nbsp;верхнем меню в&nbsp;интерфейсе WCM появляется список конверсионных тегов,
-                            сгенерированных в&nbsp;рамках аккаунта. Верхняя строка меню в&nbsp;списке конверсионных тегов показывает количество
-                            конверсий в&nbsp;столбце &laquo;#Conv.&raquo; за&nbsp;указанный период в&nbsp;правом верхнем углу и&nbsp;распределение
-                            типа подготовленного тега на&nbsp;формат:
-                            <br />
-                            <br />
-                            <span className="section__lead section__lead_color" style={styles}>
-                                IM&nbsp;(image), IM-SSL (image с&nbsp;защищенным протоколом), тоже самое для&nbsp;JS (JavaScript) и&nbsp;JS-SSL
-                                (JavaScript с&nbsp;защищенным протоколом). В&nbsp;данный момент по&nbsp;умолчанию мы&nbsp;используем только IM-SSL.
-                            </span>
-                        </p>
-                    </div>
-                    <img className="section__image" src={conversionReport} alt="отчеты по конверсиям" />
+
+                <div className="section__content">
+                    <p className="section__lead">
+                        При переходе на&nbsp;вкладку Conversion в&nbsp;верхнем меню в&nbsp;интерфейсе WCM появляется список конверсионных тегов,
+                        сгенерированных в&nbsp;рамках аккаунта. Верхняя строка меню в&nbsp;списке конверсионных тегов показывает количество конверсий
+                        в&nbsp;столбце &laquo;#Conv.&raquo; за&nbsp;указанный период в&nbsp;правом верхнем углу и&nbsp;распределение типа
+                        подготовленного тега на&nbsp;формат:
+                    </p>
+                    <p className="section__lead section__lead_color" style={styles}>
+                        IM&nbsp;(image), IM-SSL (image с&nbsp;защищенным протоколом), тоже самое для&nbsp;JS (JavaScript) и&nbsp;JS-SSL (JavaScript
+                        с&nbsp;защищенным протоколом). В&nbsp;данный момент по&nbsp;умолчанию мы&nbsp;используем только IM-SSL.
+                    </p>
                 </div>
-                <div className="section__grid-block">
-                    <img className="section__image" src={conversionReportTwo} alt="отчеты по конверсиям" />
-                    <div className="section__content">
-                        <p className="section__lead">
-                            Для просмотра данных по&nbsp;общему количеству конверсий с&nbsp;привязкой к&nbsp;рекламной кампании необходимо перейти
-                            в&nbsp;раздел Reporting по&nbsp;РК. В&nbsp;меню Select Metrics в&nbsp;разделе Basic выбрать метрику
-                            Conversion&nbsp;&mdash; затем применить условия настройки, нажав кнопку &laquo;Save and close&raquo;.
-                        </p>
-                    </div>
+                <img className="section__image" src={conversionReport} alt="отчеты по конверсиям" />
+
+                <div className="section__content">
+                    <p className="section__lead">
+                        Для просмотра данных по&nbsp;общему количеству конверсий с&nbsp;привязкой к&nbsp;рекламной кампании необходимо перейти
+                        в&nbsp;раздел Reporting по&nbsp;РК. В&nbsp;меню Select Metrics в&nbsp;разделе Basic выбрать метрику Conversion&nbsp;&mdash;
+                        затем применить условия настройки, нажав кнопку &laquo;Save and close&raquo;.
+                    </p>
                 </div>
-                <div className="section__grid-block">
-                    <div className="section__content">
-                        <p className="section__lead">
-                            Для того, чтобы отобразить статистику по&nbsp;конверсиям на&nbsp;графике, необходимо поставить галочку в&nbsp;левом меню
-                            справа от&nbsp;графика.
-                        </p>
-                    </div>
-                    <img className="section__image" src={conversionReportThree} alt="отчеты по конверсиям" />
+                <img className="section__image" src={conversionReportTwo} alt="отчеты по конверсиям" />
+                <div className="section__content">
+                    <p className="section__lead">
+                        Для того, чтобы отобразить статистику по&nbsp;конверсиям на&nbsp;графике, необходимо поставить галочку в&nbsp;левом меню
+                        справа от&nbsp;графика.
+                    </p>
                 </div>
+                <img className="section__image" src={conversionReportThree} alt="отчеты по конверсиям" />
+
                 <div className="section__content">
                     <p className="section__lead">
                         Для разбивки конверсий по&nbsp;типам необходимо перейти в&nbsp;раздел All conversion types and pages, в&nbsp;блоке Conversion
