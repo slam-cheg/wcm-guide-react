ccffefbbe8e46dd40eceea8921dea9f49135a524
Lev Shushkov <levashushkov@yandex.ru>
2022-03-16 18:36:17 +0300

    build failed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <React.StrictMode>
+    <BrowserRouter basename="/">
         <App />
-    </React.StrictMode>,
+    </BrowserRouter>,
     document.getElementById("root")
 );
 
